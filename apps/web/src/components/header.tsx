8496a7d9ed979dcdb56abcbbde0c780695b0be3b
SHOMRIDDHO'S WORLD <shomriddho.world@gmail.com>
2026-05-19 23:48:38 +0600

    feat: refactor sidebar and header components for improved layout and user experience

--- apps/web/src/components/header.tsx
+++ apps/web/src/components/header.tsx
@@ -3,27 +3,12 @@ import { ThemeTogglerButton } from "@PressScopeBd/ui/components/animate-ui/compo
 import { SidebarTrigger } from "@PressScopeBd/ui/components/sidebar";
 
 export default function Header() {
-  const links = [
-    { to: "/", label: "Home" },
-    { to: "/settings", label: "Settings" },
-    { to: "/user-info", label: "Profile" },
-  ] as const;
   const isLoginPage = window.location.pathname === "/login";
   return (
     <div>
       <div className="flex flex-row items-center justify-between px-2 py-1">
         <div className="flex items-center gap-2">
           {!isLoginPage && <SidebarTrigger />}
-
-          <nav className="flex gap-4 text-lg">
-            {links.map(({ to, label }) => {
-              return (
-                <Link key={to} to={to}>
-                  {label}
-                </Link>
-              );
-            })}
-          </nav>
         </div>
         <div className="flex items-center gap-2">
           <ThemeTogglerButton direction="btt" modes={["light", "dark"]} />
