3bb5590d08a3347ddad06cecbb42833e7a83a115
SHOMRIDDHO'S WORLD <shomriddho.world@gmail.com>
2026-05-22 01:35:37 +0600

    feat: update Playwright configuration and add E2E testing workflow

--- apps/web/playwright.config.ts
+++ apps/web/playwright.config.ts
@@ -5,14 +5,13 @@ export default defineConfig({
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 1 : 0,
-  workers: 1,
+  workers: 10,
   reporter: "html",
   use: {
     baseURL: "http://localhost:3001",
     trace: "on-first-retry",
     video: "on",
     screenshot: "only-on-failure",
-    httpsOmitScriptRequiringTrust: true,
   },
   projects: [
     {
@@ -26,6 +25,27 @@ export default defineConfig({
     {
       name: "webkit",
       use: { ...devices["Desktop Safari"] },
+    },
+    {
+      name: "desktop-edge",
+      use: { ...devices["Desktop Edge"] },
+    },
+    {
+      name: "mobile-chrome",
+      use: { ...devices["Pixel 5"] },
+    },
+    {
+      name: "mobile-safari",
+      use: { ...devices["iPhone 12"] },
+    },
+    {
+      name: "chromium-560x320",
+      use: {
+        ...devices["Desktop Chrome"],
+        // It is important to define the `viewport` property after destructuring `devices`,
+        // since devices also define the `viewport` for that device.
+        viewport: { width: 1280, height: 720 },
+      },
     },
   ],
   webServer: {
