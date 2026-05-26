603a96dfb012595d281d1bf9661628c57f44759b
SHOMRIDDHO'S WORLD <shomriddho.world@gmail.com>
2026-05-27 05:02:53 +0600

    feat: implement user management features including admin creation and role assignments

--- apps/web/src/lib/auth-client.ts
+++ apps/web/src/lib/auth-client.ts
@@ -1,6 +1,7 @@
 import { env } from "@PressScopeBd/env/web";
 import { createAuthClient } from "better-auth/react";
-
+import { adminClient } from "better-auth/client/plugins";
 export const authClient = createAuthClient({
   baseURL: env.VITE_SERVER_URL,
+  plugins: [adminClient()],
 });
